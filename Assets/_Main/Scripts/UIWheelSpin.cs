4a05a503e4a2dfb67b91881005cf00970e8c1360
Kelvin <144894229+KelvinAlviero@users.noreply.github.com>
2026-08-06 18:18:11 +0700

    FIX: Wheel wobble less noticable

--- Assets/_Main/Scripts/UIWheelSpin.cs
+++ Assets/_Main/Scripts/UIWheelSpin.cs
@@ -370,21 +370,22 @@ public class UIWheelSpin : UIPage
 
     public void OnSpinPaidButtonClicked()
     {
+        SpinningScript.Rotate();
 
-        if (flagAmount == 0)
-        {
-            balanceError.Show();
-        }
-        else
-        {
+        // if (flagAmount == 0)
+        // {
+        //     balanceError.Show();
+        // }
+        // else
+        // {
 
-        isSpinning = true;
-        closeButton.interactable = false;
-        spinFree.interactable = false;
-        spinPaid.interactable = false;
-        MissionButton.interactable = false;
-        APIController.StartSpinPaid();
-        }
+        // isSpinning = true;
+        // closeButton.interactable = false;
+        // spinFree.interactable = false;
+        // spinPaid.interactable = false;
+        // MissionButton.interactable = false;
+        // APIController.StartSpinPaid();
+        // }
     }
 
     public void OnResetFreeSpinButtonClicked()
