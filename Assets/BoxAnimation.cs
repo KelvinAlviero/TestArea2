be8fa53f13cbbef33a376432d71c7f13d016f419
Kelvin <144894229+KelvinAlviero@users.noreply.github.com>
2026-08-07 10:42:13 +0700

    Box anim

--- Assets/BoxAnimation.cs
+++ Assets/BoxAnimation.cs
@@ -1,18 +1,24 @@
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UIElements;
 
 public class BoxAnimation : MonoBehaviour
 {
-    private RectTransform boxImage;
-    private float topMax;
-    private float bottomMax;
-    private float animSpeed;
-    private float smoothSpeed;
+    [SerializeField] private RectTransform boxImage;
+    [SerializeField] private float topMax;
+    [SerializeField] private float bottomMax;
+    [SerializeField] private float animSpeed;
+    [SerializeField] private float smoothSpeed;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         DOTween.Init();
+        boxImage.transform.DOMoveY(topMax, animSpeed)
+            .SetLoops(-1, LoopType.Yoyo);
+        // boxImage.transform.DORestart();
+        // boxImage.transform.DOMoveY(bottomMax, animSpeed);
+        DOTween.Play(boxImage);
     }
 
     // Update is called once per frame
